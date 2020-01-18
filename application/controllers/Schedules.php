6f05ba6f665a2d43ee2fdb5ac6585c4c5ab4ef0f
napbie <napbie17@gmail.com>
2020-01-18 18:13:23 +0800

    nap

--- application/controllers/Schedules.php
+++ application/controllers/Schedules.php
@@ -18,5 +18,41 @@ class Schedules extends CI_Controller {
         // $data['js'] = array('pages/users.js');
         $this->load->view('layout/main', $data);
     }
+
+    public function albergo()
+	{
+        $data['title'] = "Schedules";
+        $data['vueid'] = "schedules";
+        $data['vfile'] = "page/schedules/albergo";
+        // $data['js'] = array('pages/users.js');
+        $this->load->view('layout/main', $data);
+    }
+
+    public function buyagan()
+	{
+        $data['title'] = "Schedules";
+        $data['vueid'] = "schedules";
+        $data['vfile'] = "page/schedules/buyagan";
+        // $data['js'] = array('pages/users.js');
+        $this->load->view('layout/main', $data);
+    }
+
+    public function arcadian()
+	{
+        $data['title'] = "Schedules";
+        $data['vueid'] = "schedules";
+        $data['vfile'] = "page/schedules/arcadian";
+        // $data['js'] = array('pages/users.js');
+        $this->load->view('layout/main', $data);
+    }
+
+    public function itogon()
+	{
+        $data['title'] = "Schedules";
+        $data['vueid'] = "schedules";
+        $data['vfile'] = "page/schedules/itogon";
+        // $data['js'] = array('pages/users.js');
+        $this->load->view('layout/main', $data);
+    }
     
 }
