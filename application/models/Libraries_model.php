0574e88426f7e96e424026a4140d5b0503f077e3
Beverly De Vera <beverlydevera21@gmail.com>
2020-01-21 03:41:19 +0800

    added UI for libraries

---
+++ application/models/Libraries_model.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Libraries_model extends CI_Model {
+
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+}
+
+/* End of file Libraries_model.php */
+/* Location: ./application/models/Libraries_model.php */
---
+++ application/views/page/libraries/classes.php
@@ -0,0 +1,39 @@
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Classes</h3>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-bordered table-responsive-sm table-sm">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Class Name</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>All Levels Regular Class</td>
+                                    <td><span class="badge bg-success">ACTIVE</span></td>
+                                    <td><button type="button" class="btn btn-primary btn-xs"><i class="fas fa-edit"></i></i></button></td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Mighty Cubs Level 2</td>
+                                    <td><span class="badge bg-danger">INACTIVE</span></td>
+                                    <td><button type="button" class="btn btn-primary btn-xs"><i class="fas fa-edit"></i></i></button></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
