d5d1df6edf6f3b9fe703070a34b2347d60d29321
Beverly De Vera <beverlydevera21@gmail.com>
2020-01-20 21:14:59 +0800

    profile gallery

--- application/views/page/students/profile_timeline.php
+++ application/views/page/students/profile_timeline.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="<?= base_url('assets/css/timeline.css') ?>">
+<link rel="stylesheet" href="<?= base_url('assets/css/profile_timeline.css') ?>">
 <div class="container">
     <div class="row">
         <div class="col-md-12">
