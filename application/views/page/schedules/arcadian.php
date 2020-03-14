553c41c0af1fa582b0f0a235897cd964ad15fd5d
Beverly De Vera <beverlydevera21@gmail.com>
2020-03-14 22:52:47 +0800

    schedule link - arcadian

--- application/views/page/schedules/arcadian.php
+++ application/views/page/schedules/arcadian.php
@@ -55,7 +55,7 @@
                                 <td rowspan="6" class="fill-gray stroke-text">Open Mats</td>
                                 <td rowspan="3" class="fill-gray stroke-text">Open Mats</td>
                                 <td rowspan="6" class="fill-gray stroke-text">Open Mats</td>
-                                <td rowspan="3" class="fill-semi-orange"><a href="class_schedule.php"><span class="stroke-text">Level 1 Regular and Gear Drills 10 & Below</span></a></td>
+                                <td rowspan="3" class="fill-semi-orange"><a href="<?=base_url('classes/classSchedInfo/57')?>"><span class="stroke-text">Level 1 Regular and Gear Drills 10 & Below</span></a></td>
                                 <td rowspan="22" class="fill-dark-gray stroke-text">Gym Closed</span></td>
                             </tr>
                             <tr> <td class="stroke-text">9:30 - 10:00</td></tr>
@@ -63,9 +63,9 @@
                             <tr>
                                 <td class="stroke-text">10:30 - 11:00</td>
                                 <td rowspan="3" class="fill-gray stroke-text">Open Mats</td>
-                                <td rowspan="3" class="fill-light-orange"><a href="<?=base_url('classes/classHistoryInfo/1')?>"><span class="stroke-text">All Levels Regular Class</span></a></td>
-                                <td rowspan="3" class="fill-light-orange"><a href="class_schedule.php"><span class="stroke-text">All Levels Regular Class</span></a></td>
-                                <td rowspan="3" class="fill-dark-orange"><a href="class_schedule.php"><span class="stroke-text">Level 2 Regular and Gear Drills 10 & Below</span></a></td>
+                                <td rowspan="3" class="fill-light-orange"><a href="<?=base_url('classes/classSchedInfo/37')?>"><span class="stroke-text">All Levels Regular Class</span></a></td>
+                                <td rowspan="3" class="fill-light-orange"><a href="<?=base_url('classes/classSchedInfo/47')?>"><span class="stroke-text">All Levels Regular Class</span></a></td>
+                                <td rowspan="3" class="fill-dark-orange"><a href="<?=base_url('classes/classSchedInfo/58')?>"><span class="stroke-text">Level 2 Regular and Gear Drills 10 & Below</span></a></td>
                             </tr>
                             <tr> <td class="stroke-text">11:00 - 11:30</td></tr>
                             <tr> <td class="stroke-text">11:30 - 12:00</td></tr>
@@ -76,53 +76,53 @@
                                 <td rowspan="2" class="fill-gray stroke-text">Lunch Break</td>
                                 <td rowspan="3" class="fill-gray stroke-text">Lunch Break</td>
                                 <td rowspan="2" class="fill-gray stroke-text">Lunch Break</td>
-                                <td rowspan="3" class="fill-dark-blue"><a href="class_schedule.php"><span class="stroke-text">All Levels Regular and Gear Drills Teen Class</span></a></td>
+                                <td rowspan="3" class="fill-dark-blue"><a href="<?=base_url('classes/classSchedInfo/59')?>"><span class="stroke-text">All Levels Regular and Gear Drills Teen Class</span></a></td>
                             </tr>
                             <tr>
                                 <td class="stroke-text">12:30 - 1:00</td>
                             </tr>
                             <tr>
                                 <td class="stroke-text">1:00 - 1:30</td>
-                                <td rowspan="2" class="fill-green"><a href="class_schedule.php"><span class="stroke-text">Mighty Cubs Level 2</span></a></td>
-                                <td rowspan="2" class="fill-green"><a href="class_schedule.php"><span class="stroke-text">Mighty Cubs Level 2</span></a></td>
-                                <td rowspan="2" class="fill-green"><a href="class_schedule.php"><span class="stroke-text">Mighty Cubs Level 2</span></a></td>
+                                <td rowspan="2" class="fill-green"><a href="<?=base_url('classes/classSchedInfo/32')?>"><span class="stroke-text">Mighty Cubs Level 2</span></a></td>
+                                <td rowspan="2" class="fill-green"><a href="<?=base_url('classes/classSchedInfo/42')?>"><span class="stroke-text">Mighty Cubs Level 2</span></a></td>
+                                <td rowspan="2" class="fill-green"><a href="<?=base_url('classes/classSchedInfo/53')?>"><span class="stroke-text">Mighty Cubs Level 2</span></a></td>
                             </tr>
                             <tr>
                                 <td class="stroke-text">1:30 - 2:00</td>
-                                <td class="fill-cubs"><a href="class_schedule.php"><span class="stroke-text">Mighty Cubs</span></a></td>
-                                <td class="fill-cubs"><a href="class_schedule.php"><span class="stroke-text">Mighty Cubs</span></a></td>
+                                <td class="fill-cubs"><a href="<?=base_url('classes/classSchedInfo/38')?>"><span class="stroke-text">Mighty Cubs</span></a></td>
+                                <td class="fill-cubs"><a href="<?=base_url('classes/classSchedInfo/48')?>"><span class="stroke-text">Mighty Cubs</span></a></td>
                                 <td class="fill-gray stroke-text">Lunch Break</td>
                             </tr>
                             <tr>
                                 <td class="stroke-text">2:00 - 2:30</td>
-                                <td rowspan="3" class="fill-semi-orange hoverable"><a href="class_schedule.php"><span class="stroke-text">All Levels Regular Class</span></a></td>
-                                <td rowspan="3" class="fill-red"><a href="class_schedule.php"><span class="stroke-text">All Levels Sparring Class</span></a></td>
-                                <td rowspan="3" class="fill-blue"><a href="class_schedule.php"><span class="stroke-text">All Levels Pomsae Class</span></a></td>
-                                <td rowspan="3" class="fill-yellow hoverable"><a href="class_schedule.php"><span class="stroke-text">All Levels High Intensity Gears Requierd</span></a></td>
-                                <td rowspan="3" class="fill-semi-orange"><a href="class_schedule.php"><span class="stroke-text">All Levels Regular and Gear Drills</span></a></td>
-                                <td rowspan="3" class="fill-gray"><a href="class_schedule.php"><span class="stroke-text">White Belts All Ages</span></a></td>
+                                <td rowspan="3" class="fill-semi-orange hoverable"><a href="<?=base_url('classes/classSchedInfo/33')?>"><span class="stroke-text">All Levels Regular Class</span></a></td>
+                                <td rowspan="3" class="fill-red"><a href="<?=base_url('classes/classSchedInfo/39')?>"><span class="stroke-text">All Levels Sparring Class</span></a></td>
+                                <td rowspan="3" class="fill-blue"><a href="<?=base_url('classes/classSchedInfo/43')?>"><span class="stroke-text">All Levels Pomsae Class</span></a></td>
+                                <td rowspan="3" class="fill-yellow hoverable"><a href="<?=base_url('classes/classSchedInfo/49')?>"><span class="stroke-text">All Levels High Intensity Gears Requierd</span></a></td>
+                                <td rowspan="3" class="fill-semi-orange"><a href="<?=base_url('classes/classSchedInfo/54')?>"><span class="stroke-text">All Levels Regular and Gear Drills</span></a></td>
+                                <td rowspan="3" class="fill-gray"><a href="<?=base_url('classes/classSchedInfo/60')?>"><span class="stroke-text">White Belts All Ages</span></a></td>
                             </tr>
                             <tr> <td class="stroke-text">2:30 - 3:00</td></tr>
                             <tr> <td class="stroke-text">3:00 - 3:30</td></tr>
                             <tr>
                                 <td><span class="stroke-text">3:30 - 4:00</span></td>
-                                <td rowspan="3" class="fill-semi-orange"><a href="class_schedule.php"><span class="stroke-text">All Levels Regular Class</span></a></td>
-                                <td rowspan="3" class="fill-red"><a href="class_schedule.php"><span class="stroke-text">All Levels Sparring Class</span></a></td>
-                                <td rowspan="3" class="fill-blue"><a href="class_schedule.php"><span class="stroke-text">All Levels Pomsae Class</span></a></td>
-                                <td rowspan="3" class="fill-yellow"><a href="class_schedule.php"><span class="stroke-text">All Levels High Intensity Gears Requierd</span></a></td>
-                                <td rowspan="3" class="fill-semi-orange"><a href="class_schedule.php"><span class="stroke-text">All Levels Regular and Gear Drills</span></a></td>
-                                <td rowspan="3" class="fill-dark-violet"><a href="class_schedule.php"><span class="stroke-text">Blackbelts Special Training</span></a></td>
+                                <td rowspan="3" class="fill-semi-orange"><a href="<?=base_url('classes/classSchedInfo/34')?>"><span class="stroke-text">All Levels Regular Class</span></a></td>
+                                <td rowspan="3" class="fill-red"><a href="<?=base_url('classes/classSchedInfo/40')?>"><span class="stroke-text">All Levels Sparring Class</span></a></td>
+                                <td rowspan="3" class="fill-blue"><a href="<?=base_url('classes/classSchedInfo/44')?>"><span class="stroke-text">All Levels Pomsae Class</span></a></td>
+                                <td rowspan="3" class="fill-yellow"><a href="<?=base_url('classes/classSchedInfo/50')?>"><span class="stroke-text">All Levels High Intensity Gears Requierd</span></a></td>
+                                <td rowspan="3" class="fill-semi-orange"><a href="<?=base_url('classes/classSchedInfo/55')?>"><span class="stroke-text">All Levels Regular and Gear Drills</span></a></td>
+                                <td rowspan="3" class="fill-dark-violet"><a href="<?=base_url('classes/classSchedInfo/61')?>"><span class="stroke-text">Blackbelts Special Training</span></a></td>
                             </tr>
                             <tr> <td class="stroke-text">4:00 - 4:30</td></tr>
                             <tr> <td class="stroke-text">4:30 - 5:00</td></tr>
                             <tr>
                                 <td class="stroke-text">5:00 - 5:30</td>
-                                <td rowspan="3" class="fill-semi-orange"><a href="class_schedule.php"><span class="stroke-text">All Levels Regular Class</span></a></td>
-                                <td rowspan="3" class="fill-red"><a href="class_schedule.php"><span class="stroke-text">All Levels Sparring Class</span></a></td>
-                                <td rowspan="3" class="fill-blue"><a href="class_schedule.php"><span class="stroke-text">All Levels Pomsae Class</span></a></td>
-                                <td rowspan="3" class="fill-yellow"><a href="class_schedule.php"><span class="stroke-text">All Levels High Intensity Gears Requierd</span></a></td>
-                                <td rowspan="3" class="fill-semi-orange"><a href="class_schedule.php"><span class="stroke-text">All Levels Regular and Gear Drills</span></a></td>
-                                <td class="fill-gray"><a href="class_schedule.php"><span class="stroke-text">Blackbelts Club</span></a></td>
+                                <td rowspan="3" class="fill-semi-orange"><a href="<?=base_url('classes/classSchedInfo/35')?>"><span class="stroke-text">All Levels Regular Class</span></a></td>
+                                <td rowspan="3" class="fill-red"><a href="<?=base_url('classes/classSchedInfo/41')?>"><span class="stroke-text">All Levels Sparring Class</span></a></td>
+                                <td rowspan="3" class="fill-blue"><a href="<?=base_url('classes/classSchedInfo/45')?>"><span class="stroke-text">All Levels Pomsae Class</span></a></td>
+                                <td rowspan="3" class="fill-yellow"><a href="<?=base_url('classes/classSchedInfo/51')?>"><span class="stroke-text">All Levels High Intensity Gears Requierd</span></a></td>
+                                <td rowspan="3" class="fill-semi-orange"><a href="<?=base_url('classes/classSchedInfo/56')?>"><span class="stroke-text">All Levels Regular and Gear Drills</span></a></td>
+                                <td class="fill-gray"><a href="<?=base_url('classes/classSchedInfo/62')?>"><span class="stroke-text">Blackbelts Club</span></a></td>
                                 
                             </tr>
                             <tr> <td class="stroke-text">5:30 - 6:00</td><td></td></tr>
@@ -130,11 +130,10 @@
                             <tr> <td class="stroke-text">6:00 - 6:30</td></tr>
                             <tr>
                                 <td class="stroke-text">6:30 - 7:00</td>
-                                <td rowspan="2" class="fill-gray"><a href="class_schedule.php"><span class="stroke-text">Active for Life</span></a></td>
+                                <td rowspan="2" class="fill-gray"><a href="<?=base_url('classes/classSchedInfo/36')?>"><span class="stroke-text">Active for Life</span></a></td>
                                 <td></td>
-                                <td rowspan="2" class="fill-gray"><a href="class_schedule.php"><span class="stroke-text">Active for Life</span></a></td>
-                                
-                                <td rowspan="2" class="fill-gray"><a href="class_schedule.php"><span class="stroke-text">Active for Life</span></a></td>
+                                <td rowspan="2" class="fill-gray"><a href="<?=base_url('classes/classSchedInfo/46')?>"><span class="stroke-text">Active for Life</span></a></td>
+                                <td rowspan="2" class="fill-gray"><a href="<?=base_url('classes/classSchedInfo/52')?>"><span class="stroke-text">Active for Life</span></a></td>
                                 <td></td>
                                 <td></td>
                             </tr>
