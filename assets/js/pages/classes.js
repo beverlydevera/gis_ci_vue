dda375cf21e7ef6d840a7e4e44c1d28ae883e2fc
Beverly De Vera <beverlydevera21@gmail.com>
2020-03-14 22:23:44 +0800

    schedules add

--- assets/js/pages/classes.js
+++ assets/js/pages/classes.js
@@ -44,8 +44,6 @@ var classsched = new Vue({
                 month = '' + (d.getMonth() + 1),
                 day = '' + d.getDate(),
                 year = d.getFullYear();
-            // if (month.length < 2) month = '0' + month;
-            // if (day.length < 2) day = '0' + day;
             month = monthlist[month];
             var returndateformat = month + " " + day + ", " + year;
             return returndateformat;
