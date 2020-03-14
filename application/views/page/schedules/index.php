d88d07835f790d5197e5e8d6cd8bda718f9f327e
Beverly De Vera <beverlydevera21@gmail.com>
2020-03-14 22:23:07 +0800

    links for abanao schedule

--- application/views/page/schedules/index.php
+++ application/views/page/schedules/index.php
@@ -55,7 +55,7 @@
                                 <td rowspan="6" class="fill-gray stroke-text">Open Mats</td>
                                 <td rowspan="3" class="fill-gray stroke-text">Open Mats</td>
                                 <td rowspan="6" class="fill-gray stroke-text">Open Mats</td>
-                                <td rowspan="3" class="fill-gray"><a href="class_schedule.php"><span class="stroke-text">White Belts All Ages</span></a></td>
+                                <td rowspan="3" class="fill-gray"><a href="<?=base_url('classes/classSchedInfo/26')?>"><span class="stroke-text">White Belts All Ages</span></a></td>
                                 <td rowspan="22" class="fill-dark-gray stroke-text">Gym Closed</span></td>
                             </tr>
                             <tr> <td class="stroke-text">9:30 - 10:00</td></tr>
@@ -63,9 +63,9 @@
                             <tr>
                                 <td class="stroke-text">10:30 - 11:00</td>
                                 <td rowspan="3" class="fill-gray stroke-text">Open Mats</td>
-                                <td rowspan="3" class="fill-light-orange"><a href="<?=base_url('classes/classHistoryInfo/1')?>"><span class="stroke-text">All Levels Regular Class</span></a></td>
-                                <td rowspan="3" class="fill-light-orange"><a href="class_schedule.php"><span class="stroke-text">All Levels Regular Class</span></a></td>
-                                <td rowspan="3" class="fill-semi-orange"><a href="class_schedule.php"><span class="stroke-text">Level 1 Regular & Geardrills 10 and Below</span></a></td>
+                                <td rowspan="3" class="fill-light-orange"><a href="<?=base_url('classes/classSchedInfo/6')?>"><span class="stroke-text">All Levels Regular Class</span></a></td>
+                                <td rowspan="3" class="fill-light-orange"><a href="<?=base_url('classes/classSchedInfo/16')?>"><span class="stroke-text">All Levels Regular Class</span></a></td>
+                                <td rowspan="3" class="fill-semi-orange"><a href="<?=base_url('classes/classSchedInfo/27')?>"><span class="stroke-text">Level 1 Regular & Geardrills 10 and Below</span></a></td>
                             </tr>
                             <tr> <td class="stroke-text">11:00 - 11:30</td></tr>
                             <tr> <td class="stroke-text">11:30 - 12:00</td></tr>
@@ -76,63 +76,63 @@
                                 <td rowspan="3" class="fill-gray stroke-text">Lunch Break</td>
                                 <td rowspan="2" class="fill-gray stroke-text">Lunch Break</td>
                                 <td rowspan="3" class="fill-gray stroke-text">Lunch Break</td>
-                                <td rowspan="3" class="fill-dark-orange"><a href="class_schedule.php"><span class="stroke-text">Level 2 Regular & Geardrills 10 and Below</span></a></td>
+                                <td rowspan="3" class="fill-dark-orange"><a href="<?=base_url('classes/classSchedInfo/28')?>"><span class="stroke-text">Level 2 Regular & Geardrills 10 and Below</span></a></td>
                             </tr>
                             <tr>
                                 <td class="stroke-text">12:30 - 1:00</td>
                             </tr>
                             <tr>
                                 <td class="stroke-text">1:00 - 1:30</td>
-                                <td rowspan="2" class="fill-green"><a href="class_schedule.php"><span class="stroke-text">Mighty Cubs Level 2</span></a></td>
-                                <td rowspan="2" class="fill-green"><a href="class_schedule.php"><span class="stroke-text">Mighty Cubs Level 2</span></a></td>
+                                <td rowspan="2" class="fill-green"><a href="<?=base_url('classes/classSchedInfo/7')?>"><span class="stroke-text">Mighty Cubs Level 2</span></a></td>
+                                <td rowspan="2" class="fill-green"><a href="<?=base_url('classes/classSchedInfo/17')?>"><span class="stroke-text">Mighty Cubs Level 2</span></a></td>
                             </tr>
                             <tr>
                                 <td class="stroke-text">1:30 - 2:00</td>
-                                <td class="fill-cubs"><a href="class_schedule.php"><span class="stroke-text">Mighty Cubs</span></a></td>
-                                <td class="fill-cubs"><a href="class_schedule.php"><span class="stroke-text">Mighty Cubs</span></a></td>
-                                <td class="fill-cubs"><a href="class_schedule.php"><span class="stroke-text">Mighty Cubs</span></a></td>
+                                <td class="fill-cubs"><a href="<?=base_url('classes/classSchedInfo/1')?>"><span class="stroke-text">Mighty Cubs</span></a></td>
+                                <td class="fill-cubs"><a href="<?=base_url('classes/classSchedInfo/11')?>"><span class="stroke-text">Mighty Cubs</span></a></td>
+                                <td class="fill-cubs"><a href="<?=base_url('classes/classSchedInfo/21')?>"><span class="stroke-text">Mighty Cubs</span></a></td>
                                 <td class="fill-gray stroke-text">Lunch Break</td>
                             </tr>
                             <tr>
                                 <td class="stroke-text">2:00 - 2:30</td>
-                                <td rowspan="3" class="fill-blue hoverable"><a href="class_schedule.php"><span class="stroke-text">All Levels Pomsae Class</span></a></td>
-                                <td rowspan="3" class="fill-semi-orange hoverable"><a href="class_schedule.php"><span class="stroke-text">All Levels Regular & Geardrills</span></a></td>
-                                <td rowspan="3" class="fill-yellow hoverable"><a href="class_schedule.php"><span class="stroke-text">All Levels High Intensity Gears Requierd</span></a></td>
-                                <td rowspan="3" class="fill-semi-orange hoverable"><a href="class_schedule.php"><span class="stroke-text">All Levels Regular & Geardrills</span></a></td>
-                                <td rowspan="3" class="fill-red"><a href="class_schedule.php"><span class="stroke-text">All Levels Sparring Class</span></a></td>
-                                <td rowspan="3" class="fill-dark-blue"><a href="class_schedule.php"><span class="stroke-text">All Levels Regular & Geardrills Teen Class</span></a></td>
+                                <td rowspan="3" class="fill-blue hoverable"><a href="<?=base_url('classes/classSchedInfo/2')?>"><span class="stroke-text">All Levels Pomsae Class</span></a></td>
+                                <td rowspan="3" class="fill-semi-orange hoverable"><a href="<?=base_url('classes/classSchedInfo/8')?>"><span class="stroke-text">All Levels Regular & Geardrills</span></a></td>
+                                <td rowspan="3" class="fill-yellow hoverable"><a href="<?=base_url('classes/classSchedInfo/12')?>"><span class="stroke-text">All Levels High Intensity Gears Requierd</span></a></td>
+                                <td rowspan="3" class="fill-semi-orange hoverable"><a href="<?=base_url('classes/classSchedInfo/18')?>"><span class="stroke-text">All Levels Regular & Geardrills</span></a></td>
+                                <td rowspan="3" class="fill-red"><a href="<?=base_url('classes/classSchedInfo/22')?>"><span class="stroke-text">All Levels Sparring Class</span></a></td>
+                                <td rowspan="3" class="fill-dark-blue"><a href="<?=base_url('classes/classSchedInfo/29')?>"><span class="stroke-text">All Levels Regular & Geardrills Teen Class</span></a></td>
                             </tr>
                             <tr> <td class="stroke-text">2:30 - 3:00</td></tr>
                             <tr> <td class="stroke-text">3:00 - 3:30</td></tr>
                             <tr>
                                 <td><span class="stroke-text">3:30 - 4:00</span></td>
-                                <td rowspan="3" class="fill-blue"><a href="class_schedule.php"><span class="stroke-text">All Levels Pomsae Class</span></a></td>
-                                <td rowspan="3" class="fill-semi-orange"><a href="class_schedule.php"><span class="stroke-text">All Levels Regular & Geardrills</span></a></td>
-                                <td rowspan="3" class="fill-yellow"><a href="class_schedule.php"><span class="stroke-text">All Levels High Intensity Gears Requierd</span></a></td>
-                                <td rowspan="3" class="fill-semi-orange"><a href="class_schedule.php"><span class="stroke-text">All Levels Regular & Geardrills</span></a></td>
-                                <td rowspan="3" class="fill-red"><a href="class_schedule.php"><span class="stroke-text">All Levels Sparring Class</span></a></td>
-                                <td rowspan="3" class="fill-gray"><a href="class_schedule.php"><span class="stroke-text">Lions Team Kyorugi</span></a></td>
+                                <td rowspan="3" class="fill-blue"><a href="<?=base_url('classes/classSchedInfo/3')?>"><span class="stroke-text">All Levels Pomsae Class</span></a></td>
+                                <td rowspan="3" class="fill-semi-orange"><a href="<?=base_url('classes/classSchedInfo/9')?>"><span class="stroke-text">All Levels Regular & Geardrills</span></a></td>
+                                <td rowspan="3" class="fill-yellow"><a href="<?=base_url('classes/classSchedInfo/13')?>"><span class="stroke-text">All Levels High Intensity Gears Requierd</span></a></td>
+                                <td rowspan="3" class="fill-semi-orange"><a href="<?=base_url('classes/classSchedInfo/19')?>"><span class="stroke-text">All Levels Regular & Geardrills</span></a></td>
+                                <td rowspan="3" class="fill-red"><a href="<?=base_url('classes/classSchedInfo/23')?>"><span class="stroke-text">All Levels Sparring Class</span></a></td>
+                                <td rowspan="3" class="fill-gray"><a href="<?=base_url('classes/classSchedInfo/30')?>"><span class="stroke-text">Lions Team Kyorugi</span></a></td>
                             </tr>
                             <tr> <td class="stroke-text">4:00 - 4:30</td></tr>
                             <tr> <td class="stroke-text">4:30 - 5:00</td></tr>
                             <tr>
                                 <td class="stroke-text">5:00 - 5:30</td>
-                                <td rowspan="3" class="fill-blue"><a href="class_schedule.php"><span class="stroke-text">All Levels Pomsae Class</span></a></td>
-                                <td rowspan="3" class="fill-semi-orange"><a href="class_schedule.php"><span class="stroke-text">All Levels Regular & Geardrills</span></a></td>
-                                <td rowspan="3" class="fill-yellow"><a href="class_schedule.php"><span class="stroke-text">All Levels High Intensity Gears Requierd</span></a></td>
-                                <td rowspan="3" class="fill-semi-orange"><a href="class_schedule.php"><span class="stroke-text">All Levels Regular & Geardrills</span></a></td>
-                                <td rowspan="3" class="fill-red"><a href="class_schedule.php"><span class="stroke-text">All Levels Sparring Class</span></a></td>
-                                <td rowspan="3" class="fill-gray"><a href="class_schedule.php"><span class="stroke-text">Lions Team Pomsae</span></a></td>
+                                <td rowspan="3" class="fill-blue"><a href="<?=base_url('classes/classSchedInfo/4')?>"><span class="stroke-text">All Levels Pomsae Class</span></a></td>
+                                <td rowspan="3" class="fill-semi-orange"><a href="<?=base_url('classes/classSchedInfo/10')?>"><span class="stroke-text">All Levels Regular & Geardrills</span></a></td>
+                                <td rowspan="3" class="fill-yellow"><a href="<?=base_url('classes/classSchedInfo/14')?>"><span class="stroke-text">All Levels High Intensity Gears Requierd</span></a></td>
+                                <td rowspan="3" class="fill-semi-orange"><a href="<?=base_url('classes/classSchedInfo/20')?>"><span class="stroke-text">All Levels Regular & Geardrills</span></a></td>
+                                <td rowspan="3" class="fill-red"><a href="<?=base_url('classes/classSchedInfo/24')?>"><span class="stroke-text">All Levels Sparring Class</span></a></td>
+                                <td rowspan="3" class="fill-gray"><a href="<?=base_url('classes/classSchedInfo/31')?>"><span class="stroke-text">Lions Team Pomsae</span></a></td>
                             </tr>
                             <tr> <td class="stroke-text">5:30 - 6:00</td></tr>
                             <tr> <td class="stroke-text">6:00 - 6:30</td></tr>
                             <tr>
                                 <td class="stroke-text">6:30 - 7:00</td>
-                                <td rowspan="2" class="fill-gray"><a href="class_schedule.php"><span class="stroke-text">Active for Life</span></a></td>
+                                <td rowspan="2" class="fill-gray"><a href="<?=base_url('classes/classSchedInfo/5')?>"><span class="stroke-text">Active for Life</span></a></td>
                                 <td></td>
-                                <td rowspan="2" class="fill-gray"><a href="class_schedule.php"><span class="stroke-text">Active for Life</span></a></td>
+                                <td rowspan="2" class="fill-gray"><a href="<?=base_url('classes/classSchedInfo/15')?>"><span class="stroke-text">Active for Life</span></a></td>
                                 <td></td>
-                                <td rowspan="2" class="fill-gray"><a href="class_schedule.php"><span class="stroke-text">Active for Life</span></a></td>
+                                <td rowspan="2" class="fill-gray"><a href="<?=base_url('classes/classSchedInfo/25')?>"><span class="stroke-text">Active for Life</span></a></td>
                                 <td></td>
                             </tr>
                             <tr>
